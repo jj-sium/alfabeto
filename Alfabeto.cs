ca490c7b33bfea68191dd3f18e0783c2156a8a94
Johan Valentino Ghinelli <116793076+jj-sium@users.noreply.github.com>
2022-12-16 14:21:34 +0100

    Add files via upload

---
+++ Alfabeto.cs
@@ -0,0 +1,27 @@
+﻿using System;
+// write in 2019 by maurizio.conti@fablabromagna.org
+
+public static class Alfabeto
+{
+    public static bool Verifica(string input)
+    {
+        string alfabeto = "abcdefghilmnopqrstuvz";
+        bool s = false;
+        for (int i = 0; i < alfabeto.Length; i++)
+        {
+            for (int j = 0; j < alfabeto.Length; j++)
+            {
+                if (i ==j)
+                {
+                    s = true;
+                    break;
+                }
+                else
+                {
+                    s = false;
+                }
+            }
+        }
+        return s;
+    }
+}
